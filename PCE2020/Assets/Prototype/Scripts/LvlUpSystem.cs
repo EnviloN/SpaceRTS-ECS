66e51ea6acdcad1f9e25548adc90e64e2d0a92ea
Envilon Script <envilonscript@gmail.com>
2020-11-08 08:28:59 +0100

    First test of DOTS capabilities of Unity

---
+++ PCE2020/Assets/Prototype/Scripts/LvlUpSystem.cs
@@ -0,0 +1,14 @@
+﻿using Unity.Entities;
+using Unity.Burst;
+
+public class LvlUpSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((ref LevelComponent levelComponent) =>
+        {
+            levelComponent.level += 1f * Time.DeltaTime;
+        });
+    }
+}
+
