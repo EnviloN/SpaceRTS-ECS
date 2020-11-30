a1f2c7bb3f76b4060f18c4cacb0e8ba9cda7e01b
Envilon Script <envilonscript@gmail.com>
2020-11-30 20:35:50 +0100

    Build configurations

---
+++ PCE2020/Assets/Scripts/Editor/SwitchPlatform.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+
+public class SwitchPlatform {
+
+    [MenuItem("Platform/Standalone Windows 64")]
+    public static void PerformToWindows64() {
+        // Switch to Windows standalone build.
+        EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Standalone, BuildTarget.StandaloneWindows64);
+    }
+
+    [MenuItem("Platform/WebGL")]
+    public static void PerformToWebGL() {
+        // Switch to Windows standalone build.
+        EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.WebGL, BuildTarget.WebGL);
+    }
+}
