f4c99713d417c4e216bb676168884017d55feff5
Envilon Script <envilonscript@gmail.com>
2021-01-09 20:04:04 +0100

    Broken Builds :(

--- PCE2020/Assets/Scripts/Spaceship/StarshipConfigAuthoring.cs
+++ PCE2020/Assets/Scripts/Spaceship/StarshipConfigAuthoring.cs
@@ -1,5 +1,4 @@
-﻿using Assets.Scripts.Spaceship.Flocking;
-using Unity.Entities;
+﻿using Unity.Entities;
 using UnityEngine;
 
 namespace Assets.Scripts.Spaceship {
@@ -7,7 +6,7 @@ namespace Assets.Scripts.Spaceship {
     public class StarshipConfigAuthoring : MonoBehaviour, IConvertGameObjectToEntity {
         public float CellRadius = 0.6f;
         public float PursuitRadius = 1.0f;
-        public float SeparationWeight = 2.8f;
+        public float SeparationWeight = 3.0f;
         public float AlignmentWeight = 1.0f;
         public float CohesionWeight = 1.0f;
         public float TargetWeight = 2.0f;
@@ -17,7 +16,7 @@ namespace Assets.Scripts.Spaceship {
         public float MovementSpeed = 0.8f;
 
         public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem) {
-            var boidConfig = new StarshipConfigComponent {
+            var shipConfig = new StarshipConfigComponent {
                 CellRadius = this.CellRadius,
                 PursuitRadius = this.PursuitRadius,
                 SeparationWeight = this.SeparationWeight,
@@ -29,7 +28,7 @@ namespace Assets.Scripts.Spaceship {
                 SteeringSpeed = this.SteeringSpeed,
                 MovementSpeed = this.MovementSpeed
             };
-            dstManager.AddComponentData(entity, boidConfig);
+            dstManager.AddComponentData(entity, shipConfig);
         }
     }
 }
